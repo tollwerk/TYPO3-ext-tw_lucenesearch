c94d9fe2f5557c764d41c09f878aa39c7d9aa9a1
IndyIndyIndy <IndyIndyIndy@users.noreply.github.com>
2014-01-28 14:35:10 +0100

    Update tw_luceneautocomplete.js

--- Resources/Public/Javascript/tw_luceneautocomplete.js
+++ Resources/Public/Javascript/tw_luceneautocomplete.js
@@ -1,9 +1,9 @@
 if (jQuery) {
 	jQuery(document).ready(function(){
 		jQuery('input.tx-twlucenesearch-sword').each(function() {
-			jQuery(this).autocomplete('index.php?eID=eidautosuggest', {
-		        minChars: 3,  
-		        queryParamName: 'tx_twlucenesearch_luceneautosuggest[searchterm]',
+			jQuery(this).autocomplete('index.php?eID=eidautocomplete', {
+		        minChars: 3,   
+		        queryParamName: 'tx_twlucenesearch_luceneautocomplete[searchterm]',
 		        remoteDataType: 'json'
 		    });
 		}); 
