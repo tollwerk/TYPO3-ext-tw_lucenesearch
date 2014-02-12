8c52073b1bb932af57be8cc3b4740d11357d2641
Joschi Kuphal <joschi@kuphal.net>
2014-02-12 20:59:42 +0100

    Started backend module

---
+++ Classes/Controller/ModuleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tollwerk\TwLucenesearch\Controller;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  © 2013 Dipl.-Ing. Joschi Kuphal <joschi@tollwerk.de>, tollwerk® GmbH
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Lucene backend module controller
+ *
+ * @package		tw_lucenesearch
+ * @copyright	Copyright © 2013 Dipl.-Ing. Joschi Kuphal <joschi@tollwerk.de>, tollwerk® GmbH (http://tollwerk.de)
+ * @author		Dipl.-Ing. Joschi Kuphal <joschi@tollwerk.de>
+ */
+class ModuleController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+	/**
+	 * Manage the Lucene Index in general
+	 * 
+	 * @return \void
+	 */
+	public function indexAction() {
+		
+	}
+
+	/**
+	 * Manage the index entries of a particular page
+	 * 
+	 * @return \void
+	 */
+	public function pageAction() {
+		
+	}
+
+	/**
+	 * Manage the other index entries
+	 * 
+	 * @return \void
+	 */
+	public function otherAction() {
+		
+	}
+}
+
+?>
